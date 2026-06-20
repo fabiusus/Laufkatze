785c8d3029d85eff7f1648cf0b7b05442557f9de
Marina <st188385@stud.uni-stuttgart.de>
2026-06-20 23:31:43 +0200

    update, für kleine massen tuts, tunen notwendig für massen ab 3kg

--- reglerInit.m
+++ reglerInit.m
@@ -1,108 +1,90 @@
 %% Initialisierung des Reglers
- %% Initialisierung des Reglers
-M_max = 0.4;
+
+M_max = 0.2;
+
+T = 1.5;
+G = [1;1;1;1];
+q_basis = [10000,0.01,5,800,0.01,100,0.01,100];
+Q = [diag([5000,1,50,40,0.1,10,1,10]), diag([5000,1,50,40,0.1,10,1,10]), diag([10000,0.01,5,800,0.01,100,0.01,100]), diag([10000,0.01,5,800,0.01,100,0.01,100]), diag([10000,0.01,5,800,0.01,100,0.01,100]), diag([10000,0.01,5,800,0.01,100,0.01,100])];
+S = [3000, 3000, 100, 50, 50, 50];
+
+m1_vec = [1,1.5,2.5,3.5,4.5,5];
+m1_sim = 2;
+x_soll = 1;
+% fixed parameters
+
+g=9.81;
+
 E_final = zeros(4,2);
 Kx_final = zeros(1,4);
 Kxi_final = zeros(1,1);
-T = 50.5; %muss noch angepasst werden
-
-m1 = 1;
-g=9.81;
-
-A= [0 1 0 0;
-    -g*(1+m1/0.8) 0 0 0.009;
-    0 0 0 1;
-    12.26*m1 0 0 -0.009];
+% System
+x0= [0; 0; 0; 0];
 
 B=[0; -30.49; 0; 30.49];
-
 C = eye(4);
 C_new= [1 0 0 0;
     0 0 1 0];
 C_vorfilter = [0 0 1 0];
-
 D = zeros(4,1);
 
-pole = -1*[2.5,2.6,2.3,2.4,10.1];
-% pole = -1*[1.1,1.2,1.3,1.4];
-pole_ne = -1*[2.5,2.6,2.3,2.4];
-A_erw = [A zeros(4,1);
-        -[0 0 1 0] zeros(1,1)];
-x0= [0; 0; 1; 0];
-% K = place(A,B,pole_ne);
-K3place = place(A_erw, [B ; 0], pole);
-
-%random Regler von Gemini, alter hat nicht funktioniert
-Q = [diag([30,5,2,1,3]), diag([30,5,2,1,3]), diag([30,5,2,1,3]), diag([30,5,2,1,3]), diag([30,5,2,1,3]), diag([30,5,2,1,3])]
-%Q = [diag([3,0.01,15,4,3]), diag([3,0.01,15,4,3]), diag([3,0.01,15,4,3]), diag([3,0.01,15,4,3]), diag([3,0.01,15,4,3]), diag([3,0.01,15,4,3])];
-S = [10, 10, 10, 10, 10, 10];
-Kx=zeros(6, 4);
-Kxi=zeros(6,1);
-for k = 1:6
- K_temp = lqr(A_erw,[B;0],Q(:,(((k-1)*5+1):k*5)),S(k));
- Kx(k,:) = K_temp(:,1:4);
- Kxi(k,:) = -K_temp(:,5);
-end
-
-zhut0 = [0;0;0;0];
-xi0 = 0;
-
-% E = place(A',C_new', 2*pole_ne)';
-
-poles = [eig(A-B*Kx(1,:)), eig(A-B*Kx(2,:)), eig(A - B*Kx(3,:)), eig(A - B*Kx(4,:)), eig(A - B*Kx(5,:)), eig(A - B*Kx(6,:))];
-E_temp = zeros(4,12);
-for i = 1:6
-    E_temp(:,2*i-1:2*i) = place(A', C_new', 3*poles(:,i))';
-end
-
-%%Sylvester Gleichingen
 S_sy=[0 0 0 0;
     0 0 1 0;
     0 0 0 1;
     1/T^3 -1/T^3 -3/T^2 -3/T];
+% A= [0 1 0 0;
+%     -g*(1+m1/0.8) 0 0 0.009;
+%     0 0 0 1;
+%     12.26*m1 0 0 -0.009];
+% A_erw = [A zeros(4,4);
+%         -G*C_vorfilter S_sy];
+
+B_erww = [B;0;0;0;0];
+
+Kx=zeros(6, 4);
+Kxi=zeros(6,4);
+E_temp = zeros(4, 12);
+
+obs_mult = [1.9, 1.9, 5.0, 5.0, 5.0, 5.0];
+
+
+for k = 1:6
+  
+    m1_k = m1_vec(k);
+    A_k = [0 1 0 0;
+        -g*(1+m1_k/0.8) 0 0 0.009;
+        0 0 0 1;
+        12.26*m1_k 0 0 -0.009];
+        
+    A_erw = [A_k zeros(4,4);
+            -G*C_vorfilter S_sy];
+            
+   
+    Q_k = Q(:, (((k-1)*8+1):k*8));  
+    K_temp = lqr(A_erw, B_erww, Q_k, S(k));
+    Kx(k,:) = -K_temp(:,1:4);
+    Kxi(k,:) = -K_temp(:,5:8);
+    
+    
+    poles_k = eig(A_k + B*Kx(k,:));
+    E_temp(:, 2*k-1:2*k) = place(A_k', C_new', obs_mult(k) * poles_k)';
+end
+
+zhut0 = [0;0;0;0];
+xi0 = [0;0;0;0];
+
+m1 = m1_sim;
+A = [0 1 0 0;
+    -g*(1+m1/0.8) 0 0 0.009;
+    0 0 0 1;
+    12.26*m1 0 0 -0.009];
+
+
+%%Sylvester Gleichungen
 Q_sy = [0 1 0 0];
-ff=[kron(S_sy',eye(4))-kron(eye(4),A), -kron(eye(4),B); -kron(eye(4),C_vorfilter), zeros(4,4)]\[zeros(16,1);Q_sy(:)];
+ff=[kron(S_sy',eye(4))-kron(eye(4),A), -kron(eye(4),B); kron(eye(4),C_vorfilter), zeros(4,4)]\[zeros(16,1);Q_sy(:)];
 pi_vec=ff(1:16);
 gamma_vec=ff(17:end);
 Pi=reshape(pi_vec,4,4);
 Gamma=reshape(gamma_vec,1,4);
 
-
-% 
-% %%jold stuff
-% 
-% Q1 = diag([3,0.01,15,4,3]);
-% S1 = diag(1);
-% K31 = lqr(A_erw,[B;0],Q,S);
-% Kx1 = K3(:,1:4);
-% Kxi1 = -K3(:,5);
-% 
-% Q2 = diag([3,0.01,15,4,3]);
-% S2 = diag(1);
-% K32 = lqr(A_erw,[B;0],Q,S);
-% Kx2 = K3(:,1:4);
-% Kxi2 = -K3(:,5);
-% 
-% Q3 = diag([3,0.01,15,4,3]);
-% S3 = diag(1);
-% K33 = lqr(A_erw,[B;0],Q,S);
-% Kx3 = K3(:,1:4);
-% Kxi3 = -K3(:,5);
-% 
-% Q4 = diag([3,0.01,15,4,3]);
-% S4 = diag(1);
-% K34 = lqr(A_erw,[B;0],Q,S);
-% Kx4 = K3(:,1:4);
-% Kxi4 = -K3(:,5);
-% 
-% Q5 = diag([3,0.01,15,4,3]);
-% S5 = diag(1);
-% K35 = lqr(A_erw,[B;0],Q,S);
-% Kx5 = K3(:,1:4);
-% Kxi5 = -K3(:,5);
-% 
-% Q6 = diag([3,0.01,15,4,3]);
-% S6 = diag(1);
-% K36 = lqr(A_erw,[B;0],Q,S);
-% Kx6 = K3(:,1:4);
-% Kxi6 = -K3(:,5);
--- test_init.m
+++ test_init.m
@@ -3,14 +3,15 @@ clear all
 clc
 % tunable parameters 
 
-T = 2;
+T = 1.5;
 G = [1;1;1;1];
-q_basis = [10000,0.1,100,40,0.1,1,1,1];
-Q = [diag([5000,1,50,40,0.1,10,1,10]), diag([5000,1,50,40,0.1,10,1,10]), diag(q_basis), diag(q_basis), diag(q_basis), diag(q_basis)];
-S = [3000, 3000, 5000, 50, 50, 50];
+q_basis = [10000,0.01,5,800,0.01,100,0.01,100];
+Q = [diag([5000,1,50,40,0.1,10,1,10]), diag([5000,1,50,40,0.1,10,1,10]), diag([10000,0.01,5,800,0.01,100,0.01,100]), diag([10000,0.01,5,800,0.01,100,0.01,100]), diag([10000,0.01,5,800,0.01,100,0.01,100]), diag([10000,0.01,5,800,0.01,100,0.01,100])];
+S = [3000, 3000, 100, 50, 50, 50];
 
-m1 = 2;
-x_soll = -2.45;
+m1_vec = [1,1.5,2.5,3.5,4.5,5];
+m1_sim = 3;
+x_soll = 1;
 % fixed parameters
 
 g=9.81;
@@ -20,10 +21,7 @@ Kx_final = zeros(1,4);
 Kxi_final = zeros(1,1);
 % System
 x0= [0; 0; 0; 0];
-A= [0 1 0 0;
-    -g*(1+m1/0.8) 0 0 0.009;
-    0 0 0 1;
-    12.26*m1 0 0 -0.009];
+
 B=[0; -30.49; 0; 30.49];
 C = eye(4);
 C_new= [1 0 0 0;
@@ -35,37 +33,58 @@ S_sy=[0 0 0 0;
     0 0 1 0;
     0 0 0 1;
     1/T^3 -1/T^3 -3/T^2 -3/T];
+% A= [0 1 0 0;
+%     -g*(1+m1/0.8) 0 0 0.009;
+%     0 0 0 1;
+%     12.26*m1 0 0 -0.009];
+% A_erw = [A zeros(4,4);
+%         -G*C_vorfilter S_sy];
 
-A_erw = [A zeros(4,4);
-        -G*C_vorfilter S_sy];
 B_erww = [B;0;0;0;0];
 
 Kx=zeros(6, 4);
 Kxi=zeros(6,4);
+E_temp = zeros(4, 12);
+
+obs_mult = [1.9, 1.9, 5.0, 5.0, 5.0, 5.0];
+
 
 for k = 1:6
- Q_k = Q(:, (((k-1)*8+1):k*8));  
- K_temp = lqr(A_erw,B_erww,Q_k,S(k));
- Kx(k,:) = - K_temp(:,1:4);
- Kxi(k,:) = - K_temp(:,5:8);
+  
+    m1_k = m1_vec(k);
+    A_k = [0 1 0 0;
+        -g*(1+m1_k/0.8) 0 0 0.009;
+        0 0 0 1;
+        12.26*m1_k 0 0 -0.009];
+        
+    A_erw = [A_k zeros(4,4);
+            -G*C_vorfilter S_sy];
+            
+   
+    Q_k = Q(:, (((k-1)*8+1):k*8));  
+    K_temp = lqr(A_erw, B_erww, Q_k, S(k));
+    Kx(k,:) = -K_temp(:,1:4);
+    Kxi(k,:) = -K_temp(:,5:8);
+    
+    
+    poles_k = eig(A_k + B*Kx(k,:));
+    E_temp(:, 2*k-1:2*k) = place(A_k', C_new', obs_mult(k) * poles_k)';
 end
 
 zhut0 = [0;0;0;0];
 xi0 = [0;0;0;0];
 
-% E = place(A',C_new', 2*pole_ne)';
+m1 = m1_sim;
+A = [0 1 0 0;
+    -g*(1+m1/0.8) 0 0 0.009;
+    0 0 0 1;
+    12.26*m1 0 0 -0.009];
 
-poles = [eig(A+B*Kx(1,:)), eig(A+B*Kx(2,:)), eig(A + B*Kx(3,:)), eig(A + B*Kx(4,:)), eig(A + B*Kx(5,:)), eig(A + B*Kx(6,:))];
-E_temp = zeros(4,12);
-for i = 1:6
-    E_temp(:,2*i-1:2*i) = place(A', C_new', 1.9*poles(:,i))';
-end
 
 %%Sylvester Gleichungen
 Q_sy = [0 1 0 0];
-ff=[kron(S_sy',eye(4))-kron(eye(4),A), -kron(eye(4),B); -kron(eye(4),C_vorfilter), zeros(4,4)]\[zeros(16,1);Q_sy(:)];
+ff=[kron(S_sy',eye(4))-kron(eye(4),A), -kron(eye(4),B); kron(eye(4),C_vorfilter), zeros(4,4)]\[zeros(16,1);Q_sy(:)];
 pi_vec=ff(1:16);
 gamma_vec=ff(17:end);
 Pi=reshape(pi_vec,4,4);
 Gamma=reshape(gamma_vec,1,4);
-
