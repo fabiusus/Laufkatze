b00889c30e071c97b438eb4775ce3f8492f2b285
Marina <st188385@stud.uni-stuttgart.de>
2026-06-21 02:03:57 +0200

    to do: tune parameters!!!

--- test_init.m
+++ test_init.m
@@ -3,15 +3,18 @@ clear all
 clc
 % tunable parameters 
 
-T = 1.5;
+T = [3,3,1.5,2,2.3,3];
+m1_sim = 1.1;
+x_soll = 0.2;
+Q = [diag([5000,1,500,400,0.1,10,1,10]), diag([5000,1,500,400,0.1,10,1,10]), diag([10000,0.01,5,800,0.01,100,0.01,100]), diag([10000,0.001,50,800,0.01,100,0.01,100]), diag([10000,0.01,5,800,0.01,100,0.01,100]), diag([10000,0.01,5,800,0.01,100,0.01,100])];
+S = [3000, 3000, 5000, 20, 20, 20];
+obs_mult = [2, 2, 5.0, 5.0, 7.0, 7.0];
+
 G = [1;1;1;1];
 q_basis = [10000,0.01,5,800,0.01,100,0.01,100];
-Q = [diag([5000,1,50,40,0.1,10,1,10]), diag([5000,1,50,40,0.1,10,1,10]), diag([10000,0.01,5,800,0.01,100,0.01,100]), diag([10000,0.01,5,800,0.01,100,0.01,100]), diag([10000,0.01,5,800,0.01,100,0.01,100]), diag([10000,0.01,5,800,0.01,100,0.01,100])];
-S = [3000, 3000, 100, 50, 50, 50];
 
 m1_vec = [1,1.5,2.5,3.5,4.5,5];
-m1_sim = 3;
-x_soll = 1;
+
 % fixed parameters
 
 g=9.81;
@@ -29,10 +32,7 @@ C_new= [1 0 0 0;
 C_vorfilter = [0 0 1 0];
 D = zeros(4,1);
 
-S_sy=[0 0 0 0;
-    0 0 1 0;
-    0 0 0 1;
-    1/T^3 -1/T^3 -3/T^2 -3/T];
+
 % A= [0 1 0 0;
 %     -g*(1+m1/0.8) 0 0 0.009;
 %     0 0 0 1;
@@ -46,17 +46,23 @@ Kx=zeros(6, 4);
 Kxi=zeros(6,4);
 E_temp = zeros(4, 12);
 
-obs_mult = [1.9, 1.9, 5.0, 5.0, 5.0, 5.0];
 
-
+zhut0 = [0;0;0;0];
+xi0 = [0;0;0;0];
+Q_sy = [0 1 0 0];
+Pi_temp = zeros(4, 24);   
+Gamma_temp = zeros(6, 4); 
 for k = 1:6
-  
+    T_k = T(k);
     m1_k = m1_vec(k);
     A_k = [0 1 0 0;
         -g*(1+m1_k/0.8) 0 0 0.009;
         0 0 0 1;
         12.26*m1_k 0 0 -0.009];
-        
+    S_sy=[0 0 0 0;
+        0 0 1 0;
+        0 0 0 1;
+        1/T_k^3 -1/T_k^3 -3/T_k^2 -3/T_k];
     A_erw = [A_k zeros(4,4);
             -G*C_vorfilter S_sy];
             
@@ -65,14 +71,17 @@ for k = 1:6
     K_temp = lqr(A_erw, B_erww, Q_k, S(k));
     Kx(k,:) = -K_temp(:,1:4);
     Kxi(k,:) = -K_temp(:,5:8);
+
     
     
     poles_k = eig(A_k + B*Kx(k,:));
     E_temp(:, 2*k-1:2*k) = place(A_k', C_new', obs_mult(k) * poles_k)';
-end
 
-zhut0 = [0;0;0;0];
-xi0 = [0;0;0;0];
+    ff=[kron(S_sy',eye(4))-kron(eye(4),A_k), -kron(eye(4),B); kron(eye(4),C_vorfilter), zeros(4,4)]\[zeros(16,1);Q_sy(:)];
+   
+    Pi_temp(:, 4*k-3:4*k) = reshape(ff(1:16), 4, 4);
+    Gamma_temp(k, :) = reshape(ff(17:end), 1, 4);
+end
 
 m1 = m1_sim;
 A = [0 1 0 0;
@@ -80,11 +89,3 @@ A = [0 1 0 0;
     0 0 0 1;
     12.26*m1 0 0 -0.009];
 
-
-%%Sylvester Gleichungen
-Q_sy = [0 1 0 0];
-ff=[kron(S_sy',eye(4))-kron(eye(4),A), -kron(eye(4),B); kron(eye(4),C_vorfilter), zeros(4,4)]\[zeros(16,1);Q_sy(:)];
-pi_vec=ff(1:16);
-gamma_vec=ff(17:end);
-Pi=reshape(pi_vec,4,4);
-Gamma=reshape(gamma_vec,1,4);
