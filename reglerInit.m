cc57ecb3ac8c31da43563df101f08f3ec39a2b31
Fabian <fabi.mock@gmx.de>
2026-06-25 17:21:34 +0200

    jetzt gehts auch echt

--- reglerInit.m
+++ reglerInit.m
@@ -1,126 +1,87 @@
-% Simulationen fuer Projektwettbewerb ERT SS2026
-% 
-% Diese Datei dient als Template fuer die Wettbewerbsaufgabe.
-% 
-% Bitte Verwenden Sie in Ihrer Simulink-Datei keine Umlaute oder 
-% Sonderzeichen. Ansonsten kann es bei der Abgabe Probleme mit dem
-% Zeichencode geben.
-
-clear 
-close all
-
-%% Parameter 
-g = 9.81; %Erdanziehung in m/s^2
-
-
-% Schlitten
-ms = 0.8; % Masse Schlitten in kg
-Mmax=0.2; % Maximales Motordrehmoment in Nm
-wMmax=17.8; % Maximale Motorumdrehungszahl in rad/s
-Pmax=wMmax*Mmax; % Maximale elektrische Motor-Leistung (Watt)
-umax=100; % Maximale Leistungsstufe des Motors in Prozent (gross)
-
-% Pendel 
-l = 1; % Länge des Pendels in m
-dp = 0; % Reibung des Pendels
-phi_max = pi/4; % maximale Auslenkung Pendel
-
-% Rad
-ds = 7.2e-3; % Rollwiderstandskoeffizient
-r = 0.041; % Radradius in m
-
-% Kran
-L = 5; % Länge des Kranauslegers in m
-
-% Stoerung
-Stoerung1 = .1; % .1 = Stoerung ein, 0 = aus
-stoerungNoisePower = 1;
-rng('shuffle');
-stoerungSeed_1 = round((1e4-1)*rand(1));
-
-% Anfangsbedingungen
-
-xStart = 0; % Startposition des Roboters
-x_dotStart = 0; % Startgeschwindigkeit des Roboters
-phiStart = 0; % Anfaenglicher Pendelwinkel
-phi_dotStart = 0; % Startgeschwindigkeit Pendel
-
-
 %% Initialisierung des Reglers
-tic
-reglerInit 
-
-IC = [phiStart, phi_dotStart, xStart, x_dotStart]; %Vektor mit Anfangsbedingungen 
-
-%% Simulation
-tmax = 10000; % Maximal erlaubte Zeit in s (600 für Präsentationsfolie)
-tSim = [0,tmax]; % Zeitintervall über das simuliert wird
-
-simIn = Simulink.SimulationInput("simulation");
-simIn = setModelParameter(simIn,"StopTime",num2str(tmax));
-simIn = setModelParameter(simIn,"StartTime",num2str(0));
-simIn = setModelParameter(simIn,"TimeOut",600);
-simOut = sim(simIn);
-disp('Simulation beendet')
-toc
-disp('Endzeit:')
-simOut.tout(end,1)
-disp('constraint verletzt?')
-simOut.constraint_violation(1,2)
 
 
-%% Plots der Simulationsergebnisse
-close all
+T = [0.34, 0.34, 0.34, 0.42, 0.48, 0.46];
 
-phiSim = simOut.yout(:,1);
-phidotSim = simOut.yout(:,2);
-xSim = simOut.yout(:,3);
-xdotSim = simOut.yout(:,4);
-xsollSim = simOut.yout(:,5); 
-xdiffSim = simOut.yout(:,6); 
+Q = { [900,8,800, 50,0.05], ...
+      [900,8,800, 90,0.05], ...
+      [900,8,800,140,0.05], ...
+      [900,8,800,140,0.05], ...
+      [900,8,800, 90,0.05], ...
+      [900,8,1200,140,0.05] };
+S = [600, 600, 600, 600, 600, 1200];
+obs_mult = [9, 9, 9, 9, 9, 9];  
 
-figure('Name','Zustaende')
-subplot(2,2,1)
-hold all
-plot(simOut.tout,180/pi*phiSim)
-plot([simOut.tout(1),simOut.tout(end)],[phi_max*180/pi,phi_max*180/pi],'Color','r');
-plot([simOut.tout(1),simOut.tout(end)],[-phi_max*180/pi,-phi_max*180/pi],'Color','r');
-title('Pendelwinkel in Grad')
-xlabel('time in s')
-subplot(2,2,2)
-hold all
-plot(simOut.tout,phidotSim)
-title('Drehgeschwindigkeit Pendel in rad/s');
-xlabel('time in s')
-subplot(2,2,3)
-hold all
-plot(simOut.tout,xSim)
-title('Position Schlitten in m');
-plot([simOut.tout(1),simOut.tout(end)],[L/2,L/2],'Color','r');
-plot([simOut.tout(1),simOut.tout(end)],[-L/2,-L/2],'Color','r');
-xlabel('time in s')
-subplot(2,2,4)
-hold all
-plot(simOut.tout,xdotSim)
-title('Geschwindigkeit Schlitten in m/s');
-xlabel('time in s')
+% ---- one-sided rail-safety guard params (Controller03Synth) ----
+x_aim     = 2.43;     % reference setback: aim a few mm inside the rail
+x_wall    = 2.44;     % virtual-wall trigger position
+kwall     = 300.0;    % quadratic virtual-wall gain
+kwall_lin = 15.0;     % linear virtual-wall gain
+kdamp     = 8.0;      % outward-velocity damping gain
+mclip     = 0.4;      % torque clip [N*m]  (== H.MMAX)
 
-figure('Name','Referenzposition')
-hold all
-grid on
-plot(simOut.tout,xSim)
-plot(simOut.tout,xsollSim)
-legend({'Position', 'Referenzposition'})
-title('Position Schlitten und Refernzpositions')
-xlabel('time in s')
-ylabel('in m')
+m1_vec = [1, 1.5, 2.5, 3.5, 4.5, 5.5];
 
-figure('Name','Abweichung Position')
-hold all
-grid on
-plot(simOut.tout,xdiffSim)
-title('Differenz zur Referenzposition')
-xlabel('time in s')
-ylabel('Differenz in m')
+% fixed parameters
+g=9.81;
+ms = 0.8;
 
-%cool
+A_grid     = zeros(4, 4, 6);
+B_grid     = zeros(4, 1, 6);
+Kx_grid    = zeros(1, 4, 6);
+Kxi_grid   = zeros(1, 6);     % SCALAR integral gain per bin
+E_grid     = zeros(4, 2, 6);
+Pi_grid    = zeros(4, 6);
+Gamma_grid = zeros(1, 6);
+Apt3_grid  = zeros(3, 3, 6);
+bpt3_grid  = zeros(3, 6);
+
+% System
+
+B=[0; -1/(0.041*1*ms); 0; 1/(0.041*ms)];
+B_erww      = [B; 0];
+C_new= [1 0 0 0;
+    0 0 1 0];
+C_vorfilter = [0 0 1 0];
+G_int = 1;
+S_sy = 0;
+x_0 = [0;0;0;0];
+xi0 = 0;
+zhut0 = [0;0;0;0];
+
+
+for k = 1:6
+    m1 = m1_vec(k);
+    A_k = [ 0,             1, 0, 0;
+           -g*(1+m1/ms),   0, 0, 0.009;
+            0,             0, 0, 1;
+            g*m1/ms,       0, 0, -0.009 ];
+    A_grid(:, :, k) = A_k;
+    B_grid(:, :, k) = B;
+
+    % --- LQI: augmented system with single integrator on position error ---
+    A_erw  = [A_k, zeros(4, 1);
+              -G_int*C_vorfilter, 0];
+    K = lqr(A_erw, B_erww, diag(Q{k}), S(k));   % 1x5
+    Kx  = -K(1:4);          % 1x4
+    Kxi = -K(5);            % scalar
+    Kx_grid(:, :, k) = Kx;
+    Kxi_grid(k)      = Kxi;
+
+    % --- Luenberger observer: poles = obs_mult * eig(A_k + B*Kx) ---
+    poles = eig(A_k + B*Kx);
+    E = place(A_k', C_new', obs_mult(k)*poles)';   % 4x2
+    E_grid(:, :, k) = E;
+
+    % --- steady-state feedforward: [-A_k,-B; C_vorfilter,0]\[0;0;0;0;1] ---
+    ff = [-A_k, -B; C_vorfilter, 0] \ [0; 0; 0; 0; 1];
+    Pi_grid(:, k)  = ff(1:4);
+    Gamma_grid(k)  = ff(5);
+
+    % --- PT3 prefilter (triple pole at -1/T), DC gain 1 ---
+    Tk = T(k);
+    Apt3_grid(:, :, k) = [ 0,         1,         0;
+                           0,         0,         1;
+                          -1/Tk^3,   -3/Tk^2,   -3/Tk ];
+    bpt3_grid(:, k)    = [0; 0; 1/Tk^3];
+end
